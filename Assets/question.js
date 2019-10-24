96bf97c1b34428bcd7522523daef65f65c64b2ff
Aqua <aqua@Aquas-MacBook-Pro.local>
2019-10-24 12:46:50 -0400

    test works

--- Assets/question.js
+++ Assets/question.js
@@ -1,55 +1,68 @@
 var Questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-        title:"What is the correct syntax for referring to an external script called ' abc.js'?",
-        choices:[" <script href='abc.js'>","<script name='abc.js'"," <script src= 'abc.js'>", ],
-        answer:" <script src='abc.js' >"
-    },
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title:
+      "What is the correct syntax for referring to an external script called ' abc.js'?",
+    choices: [
+      "<script href='abc.js'>",
+      "<script name='abc.js'",
+      " <script src= 'abc.js'>"
+    ],
+    answer: " <script src='abc.js' >"
+  },
 
-    {
-        title:"Inside which HTML element do we put the JavaScript?",
-        choices:["<js> ","<scripting>","<script>"],
-        answer:"<script>"
+  {
+    title: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<js> ", "<scripting>", "<script>"],
+    answer: "<script>"
+  },
+  {
+    title: " Which of the following attribute can hold the JavaScript version?",
+    choices: ["LANGUAGE", "SCRIPT", "VERSION"],
+    answer: "LANGUAGE"
+  },
 
-    },
-{
-    title:" Which of the following attribute can hold the JavaScript version?",
-    choices:["LANGUAGE","SCRIPT","VERSION"],
-    answer:"LANGUAGE"
-},
-
-{
-    title:"Which types of image maps can be used with JavaScript?",
-        choices:["Server-side image maps","Client-side image maps","Server-side image maps and Client-side image maps"],
-        answer:"Client-side image maps"
-},
-{
-    title:"Which of the following navigator object properties is the same in both   Netscape and IE?",
-        choices:["navigator.appCodeName","navigator.appName","navigator.appVersion"],
-        answer:"navigator.appCodeName"
-},
-{
-    title:"JavaScript is interpreted by ______.",
-        choices:["Client","Server","Object"],
-        answer:"Client"
-},
-{
-    title:"Using _______ statement is how you test for a specific condition.",
-    choices:["Select","If","Switch"],
-    answer:"If"
-},
-{
-    title:"Which tag(s) can handle mouse events in Netscape?",
-    choices:["<IMG>","<A>","<BR>"],
-    answer:"<A>"
-}
-]
+  {
+    title: "Which types of image maps can be used with JavaScript?",
+    choices: [
+      "Server-side image maps",
+      "Client-side image maps",
+      "Server-side image maps and Client-side image maps"
+    ],
+    answer: "Client-side image maps"
+  },
+  {
+    title:
+      "Which of the following navigator object properties is the same in both   Netscape and IE?",
+    choices: [
+      "navigator.appCodeName",
+      "navigator.appName",
+      "navigator.appVersion"
+    ],
+    answer: "navigator.appCodeName"
+  },
+  {
+    title: "JavaScript is interpreted by ______.",
+    choices: ["Client", "Server", "Object"],
+    answer: "Client"
+  },
+  {
+    title: "Using _______ statement is how you test for a specific condition.",
+    choices: ["Select", "If", "Switch"],
+    answer: "If"
+  },
+  {
+    title: "Which tag(s) can handle mouse events in Netscape?",
+    choices: ["<IMG>", "<A>", "<BR>"],
+    answer: "<A>"
+  }
+];
--- Assets/script.js
+++ Assets/script.js
@@ -1,42 +1,47 @@
 var startBtn = document.getElementById("start-button");
 var questionHeading = document.getElementById("question");
 var answersList = document.getElementById("answers");
-var questionNumber = 0;
+var questionNumber = -1;
 var choises;
+var answers = [];
 
-startBtn.addEventListener("click", function(event){
-    console.log("start button works")
-    document.getElementById("start-screen").classList.add("d-none");
-    document.getElementById("qa-screen").classList.remove("d-none");
+var selectedAnswer;
+
+startBtn.addEventListener("click", function(event) {
+  console.log("start button works");
+  document.getElementById("start-screen").classList.add("d-none");
+  document.getElementById("qa-screen").classList.remove("d-none");
 });
 
 function nextQuestion() {
-    question++
-    answersList.innerHTML ="";
-    questionHeading.textContent ="";
+  //if last question, grade results
+  if (questionNumber >= 9) {
+    //grade quiz
+    console.log("write some logic to grade this quiz!")
+  } else {
+      //START HERE
+      answers.push(answerList.selectedOptions)
+    console.log(answers)
+    // answersList.addEventListener("change", function(e) {
+    //     console.log(this.textContent);
+    // });
+
+    questionNumber++;
+    console.log(questionNumber)
+    questionHeading.textContent = "";
     questionHeading.textContent = Questions[questionNumber].title;
     choises = Questions[questionNumber].choices;
 
-    for (var i=0; i < choises.length; i++){
-        var newChoice = document.createElement("div");
-        newChoice.textContent = choises[i];
-        answersList.appendChild(newChoice);
-    }
-}
-
-function startGame(){
-    //if last question, grade results
-    if (questionNumber === 10) {
-        //grade quiz
-    } else {
-        nextQuestion()
+    for (var i = 0; i < choises.length; i++) {
+    //   var newChoice = document.createElement("option");
+    //   newChoice.setAttribute("value", choises[i])
+    //   newChoice.textContent = choises[i];
+    //   answersList.appendChild(newChoice);
+        document.getElementById(i).textContent = choises[i]
     }
 
+  }
 }
-
-
-
-
 
 // this is loop//
 // questionHeading.textContent = Questions[0].title;
@@ -104,79 +109,63 @@ function startGame(){
 //     answersList.appendChild(newChoice);
 // }
 
+// // timer//
+// var timeleft = 75;
+// var timer = document.getElementById("timer");
+// timer.textContent = timeleft;
 
-// timer//
-var timeleft =75;
-var timer = document.getElementById("timer");
-timer.textContent = timeleft;
+// function timeIt() {
+//   timeleft--;
+//   timer.textContent = timeleft;
+// }
+// console.log("here");
 
-function timeIt(){
-timeleft--;
-timer.textContent = timeleft;
+// //timeIt();
 
+// setInterval(timeIt, 1000);
+// // needs to be updated//
+// if (x === true) {
+//   timer.textContent = timeleft;
+// } else {
+//   timeleft - 15;
+// }
 
+// // when time goes to 0 and alert should say  "time is over"//
+// if (timeleft <= 0) {
+//   //need to show results//
+// } else {
+// }
 
-}
-console.log("here")
+// // next test should start from 75 seconds again//
+// function restart() {
+//   timeleft = 75;
+// }
 
-//timeIt();
+// //for right or wrong answers Alerts should show //
 
-setInterval(timeIt, 1000);
-// needs to be updated//
-if(x === true){
-    timer.textContent = timeleft;
+// if (answer === true) {
+//   alert = "Correct";
+// } else {
+//   alert = "Sorry, wrong!";
+// }
 
-} else {
-    timeleft-15;
-}
+// //need box to keep scores-local storage//
 
-// when time goes to 0 and alert should say  "time is over"//
-if(timeleft <= 0){
-    //need to show results//
+// var highScoreEl = document.querySelector("#highScore");
 
-} else{
-    
-}
+// //var count = localStorage.getItem("count");
 
-// next test should start from 75 seconds again//
-function restart(){
-    timeleft=75;
-}
+// addButton.addEventListener("click", function() {
+//   count++;
+//   counter.textContent = count;
 
+//   localStorage.setItem("count", count);
+// });
 
+// // to start test//
+// incrementEl.addEventListener("click", function() {
+//   count++;
+//   setCounterText();
+// });
 
-//for right or wrong answers Alerts should show //
-
- if(answer === true){
-     alert = "Correct"
- } else{
-alert = "Sorry, wrong!"
-
- }
-
-//need box to keep scores-local storage//
-
-
-
-var highScoreEl= document.querySelector("#highScore");
-
-//var count = localStorage.getItem("count"); 
-
-addButton.addEventListener("click", function() {
-  count++;
-  counter.textContent = count;
- 
-  localStorage.setItem("count", count);
- });
-
-// to start test//
- incrementEl.addEventListener("click", function() {
-    count++;
-    setCounterText();
-  });
-
-// store test results//
-
-
-
-
+// // store test results//
